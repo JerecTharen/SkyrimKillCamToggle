d4a1220fa4017ae89687eb6e063e3619274d3e88
JerecTharen <kf7ags@gmail.com>
2020-04-08 17:15:49 -0400

    init

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+
+namespace ToggleSkyrimKillCam
+{
+    class Program
+    {
+        private static string _root = @"C:\Users\kf7ag\Documents\My Games\Skyrim";
+        private static string _yesCamLocation = @"D:\Projects\ToggleSkyrimKillCam\yesCam\Skyrim.ini";
+        private static string _noCamLocation = @"D:\Projects\ToggleSkyrimKillCam\noCam\Skyrim.ini";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Switching skyrim .ini file");
+
+            Console.WriteLine("File being used");
+
+            string fileUsedContents = File.ReadAllText(_root + @"\Skyrim.ini");
+            bool isKillCamOff = fileUsedContents.Contains("bVATSdisable=1");
+
+            Console.WriteLine(fileUsedContents);
+            Console.WriteLine($"isKillCamOff: {isKillCamOff}");
+
+            bool isKillCamOn;
+
+            if(args.Length > 0)
+                if(args[0].ToLower() == "on" || args[0].ToLower() == "off")
+                    isKillCamOn = ToggleKillCam(isKillCamOff, args[0].ToLower());
+                else
+                    isKillCamOn = ToggleKillCam(isKillCamOff);
+            else
+                isKillCamOn = ToggleKillCam(isKillCamOff);
+            
+            System.Console.WriteLine(isKillCamOn ? "Kill Cam is now on." : "Kill Cam is now off.");
+            System.Console.WriteLine("Shutting down . . .");
+            Console.ReadLine();
+        }
+
+        private static bool ToggleKillCam(bool isKillCamOff, string onOff = "")
+        {
+            //returns is kill cam is on
+            bool ret = false;
+            System.Console.WriteLine("Deleting File");
+            string location = _root + @"\Skyrim.ini";
+            File.Delete(location);
+            System.Console.WriteLine("Copying file");
+            if(String.IsNullOrEmpty(onOff))
+            {
+                File.Copy(isKillCamOff ? _yesCamLocation : _noCamLocation, location);
+                ret = !isKillCamOff;
+            }
+            else if(onOff == "on")
+            {
+                File.Copy(_yesCamLocation, location);
+                ret = true;
+            }
+            else if(onOff == "off")
+            {
+                File.Copy(_noCamLocation, location);
+                ret = false;
+            }
+            return ret;
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,18 @@
+Toggles the Kill Cam on and off for Skyrim on my machine. Written in .NET Core.
+
+--INSTALLATION--
+1.Download Visual Studio (Community edition works)
+2.Install .NET Core through Visual Studio Installer
+3.Modify the static variables at the top of Program.cs with the location of where
+	 you have your Skyrim settings (usually in your user's Documents folder)
+	and where the repository is located.
+4.Use Visual Studio to build the application or run "dotnet build" through console command.
+
+--RUNNING THE APPLICATION THROUGH THE CONSOLE--
+1.Open the console application location either in powershell, through visual studio,
+	or any other command line utility.
+2. run the command "dotnet run"
+
+--RUNNING THE APPLICATION THROUGH .EXE--
+1.Open the console application location in the file explorer.
+2.Go to the bin folder and find the .exe and run it.
